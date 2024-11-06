c7b9e5128a4b3328fabc5e8af942ba47fe735586
Demyan k. <demiandiaz.dd@gmail.com>
2024-11-06 19:53:59 +0300

    Update Module_9_3.py

--- Module_9_3.py
+++ Module_9_3.py
@@ -1,10 +1,11 @@
 first = ['Strings', 'Student', 'Computers']
-second = ['Строка', 'Урбанэ', 'Компьютер']
+second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (abs(len(f) - len(s)) for f, s in zip(first, second) if len(f) != len(s))
 
-second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+second_result = (len(f) == len(s) for f, s in zip(first, second))
 
 
 print(list(first_result))
 print(list(second_result))
+
